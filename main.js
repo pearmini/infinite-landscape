af6c7a9749358f2df566c04a119d3e0582bfaf53
Bairui Su <subairui@icloud.com>
2024-11-28 13:40:31 -0500

    Add Plain (#3)

--- main.js
+++ main.js
@@ -4,33 +4,46 @@ import {randomUniform} from "d3-random";
 
 const data = {};
 
-function mountains({width, height, count}) {
-  const noiseH = randomNoise(0, (height / 4) * 3, {octaves: 4, seed: 100000, falloff: 0.5});
-  const noiseW = randomNoise(width / 8, width / 3, {octaves: 8, seed: 100000, falloff: 0.5});
-  const noiseY = randomNoise(-height / 8, height / 8, {octaves: 4, seed: 100000, falloff: 0.5});
+function mountains({width, height, count, minY = 1 / 8, maxY = 1 / 2, addNoiseY = 1, paddingX = 3 / 4}) {
+  const noiseH = randomNoise(0, (height / 8) * 5);
+  const noiseW = randomNoise(width * minY, width * maxY);
+  const noiseY = randomNoise(-height / 8, height / 8);
   let px = 0;
   const mountains = Array.from({length: count}, (_, i) => {
     const gap = randomUniform()();
     const addGap = gap < 0.1;
     const w = noiseW(i);
-    const x = px - randomUniform(0, w)() + addGap * 200;
-    const y = (height / 4) * 3 + noiseY(x);
+    const x = px - randomUniform(0, w * paddingX)() + addGap * 200;
+    const y = (height / 8) * 5 + noiseY(x) * addNoiseY;
     const h = noiseH(x + w / 2);
     px = x + w;
-    return {x, y, x1: x + w / 2, y1: y - h, x2: x + w, y2: y};
+    return {
+      x: x,
+      y: y,
+      x1: x + w / 2,
+      y1: y - h,
+      x2: x + w,
+      y2: y,
+    };
   });
-  return mountains;
+  return mountains.sort((a, b) => Math.max(a.y, a.y2) - Math.max(b.y, b.y2));
 }
 
 function render(data, {width = 1000, height = 600} = {}) {
   const svgElement = svg`<svg width=${width} height=${height}>
     <rect width=${width} height=${height} fill="#eee"></rect>
+    <line x1="0" y1=${height / 2} x2=${width} y2=${height / 2} stroke="#000"></line>
     ${mountains({width, height, count: 20}).map(
       ({x, y, x1, y1, x2, y2}) =>
-        svg`<path d=${`M${x},${y}L${x1},${y1}L${x2},${y2}Z`} fill="#ccc" fill-opacity="0.8"></path>`
+        svg`<path d=${`M${x},${y}L${x1},${y1}L${x2},${y2}Z`} fill="#ccc" stroke="#000"></path>`
     )}
+    <g transform="translate(0, ${height - height / 4})">
+    ${mountains({width, height: height / 2, count: 10, minY: 1 / 2, maxY: 3 / 4, addNoiseY: 0}).map(
+      ({x, y, x1, y1, x2, y2}) =>
+        svg`<path d=${`M${x},${y}L${x1},${y1}L${x2},${y2}Z`} fill="#ccc" stroke="#000"></path>`
+    )}
+    </g>
   </svg>`;
-
   const container = document.querySelector("#container");
   container.appendChild(svgElement);
 }
