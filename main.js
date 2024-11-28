3406f0395d52303536cf8ad843bd1750193a8d7b
Bairui Su <subairui@icloud.com>
2024-11-28 11:42:32 -0500

    Uniform Mountains (#1)

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
-import { svg } from "htl";
+import {svg} from "htl";
 
 const data = {};
 
-function render(data) {
-  console.log("render", data);
-  const container = document.querySelector("#container");
-  const svgElement = svg`<svg width="100" height="100">
-    <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
+function mountains({width, height, count}) {
+  const mountains = Array.from({length: count}, (_, i) => {
+    const w = width / count;
+    const h = height / 4;
+    const x = i * w;
+    const y = height / 2;
+    return {x, y, x1: x + w / 2, y1: y - h, x2: x + w, y2: y};
+  });
+  return mountains;
+}
+
+function render(data, {width = 1000, height = 600} = {}) {
+  const svgElement = svg`<svg width=${width} height=${height}>
+    <rect width=${width} height=${height} fill="#eee"></rect>
+    ${mountains({width, height, count: 5}).map(
+      ({x, y, x1, y1, x2, y2}) => svg`<path d=${`M${x},${y}L${x1},${y1}L${x2},${y2}Z`} fill="#ccc"></path>`
+    )}
   </svg>`;
+
+  const container = document.querySelector("#container");
   container.appendChild(svgElement);
 }
 
